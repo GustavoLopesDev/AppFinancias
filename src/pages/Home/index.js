db847ac1c8f92ddce7cddb7861ffe656ffcfbe97
Gustavo Lopes <gustavolopesdev14@gmail.com>
2025-05-30 11:18:51 -0300

    registro funcional, add list de gastos e delet de registro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,26 @@
-import {Button} from '@react-navigation/elements';
 import React, {useContext, useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+
 import {AuthContext} from '../../contexts/auth';
 
 import Header from '../../components/Header';
-import {Background, ListBalance} from './styles';
+import {Background, ListBalance, Area, Title, List} from './styles';
 
 import api from '../../services/api';
 import {format} from 'date-fns';
 
 import {useIsFocused} from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
+import HistoricoList from '../../components/HistoricoList';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Home() {
   const isFocused = useIsFocused();
   const [listBalance, setListBalance] = useState([]);
+  const [movements, setMovements] = useState([]);
 
-  const [dateMovements, setdateMovements] = useState(new Date());
+  const [dateMovements, setDateMovements] = useState(new Date());
 
   useEffect(() => {
     let isActive = true;
@@ -24,12 +28,20 @@ export default function Home() {
     async function getMovements() {
       let dateFormated = format(dateMovements, 'dd/MM/yyyy');
 
+      const receives = await api.get('/receives', {
+        params: {
+          date: dateFormated,
+        },
+      });
+
       const balance = await api.get('/balance', {
         params: {
           date: dateFormated,
         },
       });
+
       if (isActive) {
+        setMovements(receives.data);
         setListBalance(balance.data);
       }
     }
@@ -37,18 +49,49 @@ export default function Home() {
     getMovements();
 
     return () => (isActive = false);
-  }, [isFocused]);
+  }, [isFocused, dateMovements]);
 
-  const {signOut, user} = useContext(AuthContext);
+  async function handleDelete(id) {
+    try {
+      await api.delete('/receives/delete', {
+        params: {
+          item_id: id,
+        },
+      });
+
+      setDateMovements(new Date());
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Background>
-      <Header title="Minhas Movimentações" />
+      <Header title="Minhas movimentações" />
+
       <ListBalance
         data={listBalance}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         keyExtractor={item => item.tag}
         renderItem={({item}) => <BalanceItem data={item} />}
+      />
+
+      <Area>
+        <TouchableOpacity>
+          <Icon name="event" color="#121212" size={30} />
+        </TouchableOpacity>
+        <Title>Ultimas movimentações</Title>
+      </Area>
+
+      <List
+        data={movements}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <HistoricoList data={item} deleteItem={handleDelete} />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 20}}
       />
     </Background>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,3 +8,27 @@ export const Background = styled.SafeAreaView`
 export const ListBalance = styled.FlatList`
   max-height: 190px;
 `;
+export const Area = styled.View`
+  margin-top: 20px;
+  background-color: #fff;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  display: flex;
+  flex-direction: row;
+  padding-left: 20px;
+  padding-right: 14px;
+  padding-top: 14px;
+  text-align: center;
+`;
+export const Title = styled.Text`
+  margin-left: 4px;
+  color: #121212;
+  margin-bottom: 14px;
+  font-weight: bold;
+  font-size: 20px;
+`;
+export const List = styled.FlatList`
+  flex: 1;
+  background-color: #fff;
+  padding-left: 10px;
+`;
